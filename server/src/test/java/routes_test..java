1eee0978796b7dfbfba697bb7df787d2fdea83cb
DmitryNazarov13 <120417048+DmitryNazarov13@users.noreply.github.com>
2023-03-17 12:51:15 +0300

    Update routes_test..java

--- server/src/test/java/routes_test..java
+++ server/src/test/java/routes_test..java
@@ -1,69 +1,188 @@
-func TestGetProjAPIUnauthorized(t *testing.T) {
-	// create a new request with JSON body containing project ID
-	requestBody := reqProj{Proj: 3}
-	reqBodyBytes, err := json.Marshal(requestBody)
-	if err != nil {
-		t.Errorf("Failed to create request body: %v", err)
+package routes_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+
+func TestCreateFinance(t *testing.T) {
+	app := setupTestApp()
+
+	// Тестируем успешный запрос
+	finance := models.Finance{
+		Profit:   100,
+		Revenue:  200,
+		Cost:     50,
+		Taxes:    30,
+		Expenses: 20,
 	}
-	req, err := http.NewRequest("GET", "/projects", bytes.NewBuffer(reqBodyBytes))
-	if err != nil {
-		t.Errorf("Failed to create request: %v", err)
-	}
+	financeJSON, _ := json.Marshal(finance)
+	req, err := http.NewRequest("POST", "/api/v1/finances", bytes.NewBuffer(financeJSON))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
 
-	// create a new ProjsBase instance with mock database and memorydb
-	pb := &ProjsBase{}
+	// Проверяем, что ответ содержит все поля модели Finance
+	responseFinance := routes.Finance{}
+	err = json.NewDecoder(resp.Body).Decode(&responseFinance)
+	assert.NoError(t, err)
+	assert.NotZero(t, responseFinance.ID)
+	assert.Equal(t, finance.Profit, responseFinance.Profit)
+	assert.Equal(t, finance.Revenue, responseFinance.Revenue)
+	assert.Equal(t, finance.Cost, responseFinance.Cost)
+	assert.Equal(t, finance.Taxes, responseFinance.Taxes)
+	assert.Equal(t, finance.Expenses, responseFinance.Expenses)
 
-	// create a new response recorder
-	rr := httptest.NewRecorder()
-
-	// call GetProjAPI handler function
-	http.HandlerFunc(pb.GetProjAPI).ServeHTTP(rr, req)
+	// Тестируем ошибку при неправильных входных данных
+	invalidFinance := models.Finance{}
+	invalidFinanceJSON, _ := json.Marshal(invalidFinance)
+	req, err = http.NewRequest("POST", "/api/v1/finances", bytes.NewBuffer(invalidFinanceJSON))
+	assert.NoError(t, err)
+	req.Header.Set("Content-Type", "application/json")
+	resp, err = app.Test(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 400, resp.StatusCode)
 }
 
-func TestTaskHandler(t *testing.T) {
+func TestGetFinances(t *testing.T) {
+	app := setupTestApp()
 
-	tb := &TasksBase{}
+	// Создаем несколько объектов Finance в базе данных для тестирования
+	finance1 := models.Finance{
+		Profit:   100,
+		Revenue:  200,
+		Cost:     50,
+		Taxes:    30,
+		Expenses: 20,
+	}
+	database.Database.Db.Create(&finance1)
 
-	// Test creating a new item
-	t.Run("CreateNewItem", func(t *testing.T) {
-		// Create a test http request with a JSON payload
-		taskJSON := `{"Tag": "", "Category": "", "count": "date", "content": "createButton": "Создать"}`
-		req, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8090/tasks/task/new", bytes.NewBufferString(taskJSON))
-		if err != nil {
-			t.Fatal(err)
-		}
+	finance2 := models.Finance{
+		Profit:   200,
+		Revenue:  400,
+		Cost:     100,
+		Taxes:    60,
+		Expenses: 40,
+	}
+	database.Database.Db.Create(&finance2)
 
-		rr := httptest.NewRecorder()
+	// Тестируем успешный запрос
+	req, err := http.NewRequest("GET", "/api/v1/finances", nil)
+	assert.NoError(t, err)
+	resp, err := app.Test(req)
+	assert.NoError(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
 
-		// Call the task handler function
-		handler := http.HandlerFunc(tb.TaskHandler)
-		handler.ServeHTTP(rr, req)
+	// Проверяем, что ответ содержит все объекты Finance
+	responseFinances := []routes.Finance{}
+	err = json.NewDecoder(resp.Body).Decode(&responseFinances)
+	assert.NoError(t, err)
+	assert.Len(t, responseFinances, 2)
+	assert.Equal(t, finance1.Profit, responseFinances[0].Profit)
+	func TestCreateFinance(t *testing.T) {
+	// Создаем новый экземпляр приложения Fiber
+	app := fiber.New()
 
-		// Confirm the response status
-		if st := rr.Result().StatusCode; st != http.StatusSeeOther {
-			t.Errorf("handler returned wrong status code: got %v expected %v", st, http.StatusSeeOther)
-		}
-	})
+	// Создаем тестовый запрос
+	req := httptest.NewRequest(http.MethodPost, "/finance", strings.NewReader(`{"profit": 100, "revenue": 200, "cost": 50, "taxes": 20, "expenses": 30}`))
 
-	// Test creating a new category
-	t.Run("UpdateTask", func(t *testing.T) {
-		// Create a test http request with a JSON payload
-		taskJSON := `{"tag": "", "category": "", "count": "date", "content": "updateButton": "Создать"}`
-		req, err := http.NewRequest(http.MethodPost, "http://127.0.0.1:8090/tasks/task/10", bytes.NewBufferString(taskJSON))
-		if err != nil {
-			t.Fatal(err)
-		}
+	// Создаем контекст запроса
+	ctx := app.AcquireCtx(req, nil)
 
-		rr := httptest.NewRecorder()
+	// Запускаем функцию CreateFinance
+	err := CreateFinance(ctx)
 
-		// Call the task handler function
-		handler := http.HandlerFunc(tb.TaskHandler)
-		handler.ServeHTTP(rr, req)
+	// Убеждаемся, что ошибки нет
+	assert.Nil(t, err)
 
-		// Confirm the response status
-		if st := rr.Result().StatusCode; st != http.StatusSeeOther {
-			t.Errorf("handler returned wrong status code: got %v expected %v", st, http.StatusSeeOther)
-		}
-	})
+	// Проверяем, что код состояния ответа равен 200
+	assert.Equal(t, http.StatusOK, ctx.Response().StatusCode())
 
+	// Проверяем, что тело ответа содержит правильные данные
+	expected := `{"id":1,"profit":100,"revenue":200,"cost":50,"taxes":20,"expenses":30}`
+	assert.JSONEq(t, expected, ctx.Response().Body())
 }
+
+func TestGetFinances(t *testing.T) {
+	// Создаем новый экземпляр приложения Fiber
+	app := fiber.New()
+
+	// Добавляем несколько записей в базу данных
+	db := database.Database.Db
+	db.Create(&models.Finance{Profit: 100, Revenue: 200, Cost: 50, Taxes: 20, Expenses: 30})
+	db.Create(&models.Finance{Profit: 200, Revenue: 300, Cost: 100, Taxes: 50, Expenses: 50})
+
+	// Создаем тестовый запрос
+	req := httptest.NewRequest(http.MethodGet, "/finances", nil)
+
+	// Создаем контекст запроса
+	ctx := app.AcquireCtx(req, nil)
+
+	// Запускаем функцию GetFinances
+	err := GetFinances(ctx)
+
+	// Убеждаемся, что ошибки нет
+	assert.Nil(t, err)
+
+	// Проверяем, что код состояния ответа равен 200
+	assert.Equal(t, http.StatusOK, ctx.Response().StatusCode())
+
+	// Проверяем, что тело ответа содержит правильные данные
+	expected := `[{"id":1,"profit":100,"revenue":200,"cost":50,"taxes":20,"expenses":30},{"id":2,"profit":200,"revenue":300,"cost":100,"taxes":50,"expenses":50}]`
+	assert.JSONEq(t, expected, ctx.Response().Body())
+}
+
+func TestGetFinance(t *testing.T) {
+	// Создаем новый экземпляр приложения Fiber
+	app := fiber.New()
+
+	// Добавляем запись в базу данных
+	db := database.Database.Db
+	finance := models.Finance{Profit: 100, Revenue: 200, Cost: 50, Taxes: 20, Expenses: 30}
+	db.Create(&finance)
+
+	// Создаем тестовый запрос
+	req := httptest.NewRequest(http.MethodGet, "/finance/"+strconv.Itoa(int(finance.ID)), nil)
+
+	// Создаем контекст запроса
+	ctx := app.AcquireCtx(req, nil)
+
+	// Запускаем функцию GetFinance
+	err := GetFinance(ctx)
+
+	// Убеждаемся, что ошибки нет
+	assert.Nil(t, err)
+
+func TestCreateProduct(t *testing.T) {
+	// Initialize database
+	database.ConnectToTestDB()
+	database.Database.Db.AutoMigrate(&models.Product{})
+	defer database.Database.Db.Migrator().DropTable(&models.Product{})
+
+	// Create test product
+	product := models.Product{Name: "Test Product", Price: "10.99"}
+
+	// Convert product to JSON
+	jsonProduct, err := json.Marshal(product)
+	assert.NoError(t, err)
+
+	// Send POST request to /api/v1/products
+	req := httptest.NewRequest(http.MethodPost, "/api/v1/products", bytes.NewBuffer(jsonProduct))
+	req.Header.Set("Content-Type", "application/json")
+	resp, err := app.Test(req)
+	assert.NoError(t, err)
+
+	// Assert response status code and JSON response body
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	expected := Product{ID: 1, Name: product.Name, Price: product.Price}
+	var actual Product
+	err = json.NewDecoder(resp.Body).Decode(&actual)
+	assert.NoError(t, err)
+	assert.Equal(t, expected, actual)
+		}
